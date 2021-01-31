fac9bb374a07b696f8490904204b7ad7680a4e6f
greencyber18 <39981392+greencyber18@users.noreply.github.com>
2021-01-31 16:06:32 +0600

    Add files via upload

---
+++ RegForm.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Registration Page </title>
+</head>
+<body>
+<h1> REGISTRATION </h1>
+
+<form>
+<table>
+<tr><td> Name: </td>
+<td><input type="text" name="firstname"></td></tr>
+
+
+<tr><td> Email: </td>
+<td><input type="email" name="email"></td></tr>
+
+
+<tr><td> Username: </td>
+<td><input type="text" name="username"></td></tr>
+
+
+
+<tr><td> Password: </td>
+<td><input type="password" name="password"></td></tr>
+
+
+<tr><td> Confirm Password: </td>
+<td><input type="password" name="cnpassword"></td></tr>
+</table>
+<br>
+<p1><strong>Gender</strong></p1>
+<br>
+<input type="radio" id="male" name="gender">
+<label for="male">Male</label>
+<input type="radio" id="female" name="gender" >
+<label for="female">Female</label>
+<input type="radio" id="other" name="gender" >
+<label for="other">Other</label> 
+<br>
+<br>
+<p1><strong>Date Of Birth </strong></p1>
+<br>
+<input type="date" id="birthday" name="birthday">
+<br>
+<br>
+<button type="submit" form="form">Submit</button> 
+<button type="reset" form="form">Reset</button> 
+
+</form>
+
+   
+</body>
+</html>
